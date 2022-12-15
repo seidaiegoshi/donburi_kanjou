513bdc5f475a2df4b85abb40490949b224373d16
seidaiegoshi <egoshi.seidai@gmail.com>
2022-12-15 17:41:54 +0900

    HOME画面

---
+++ pages/function/footer.php
@@ -0,0 +1,30 @@
+<?php
+
+function footer()
+{
+  return "
+  <footer>
+    <div>
+      <a href='./../home/home.php'>
+        HOME
+      </a>
+    </div>
+    <div>
+      <a href='./../input/sales_performance.php'>
+        INPUT
+      </a>
+    </div>
+    <div>
+      <a href='./../fixed/fixed_cost.php'>
+        FIXED
+      </a>
+    </div>
+    <div>
+      <a href='./../analysis/monthly_graph.php'>
+        GRAPH
+      </a>    
+    </div>
+    <div>ACCOUNT</div>
+  </footer>
+";
+}
---
+++ pages/home/home.php
@@ -0,0 +1,35 @@
+<?php
+include("./../function/footer.php");
+// include("./~/pages/function/footer.php");
+$footer = footer();
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>home</title>
+  <link rel="stylesheet" type="text/css" href="./../../css/basic_style.css">
+</head>
+
+<body>
+  <header>
+    <div>
+    </div>
+    <div>
+      <h1 class="page-title">ホーム</h1>
+    </div>
+    <div>
+    </div>
+  </header>
+
+
+
+  <?= $footer ?>
+</body>
+
+</html>
