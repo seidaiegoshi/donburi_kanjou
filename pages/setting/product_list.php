7b90041c00bf252d6c26a5d46e52e1d6f8ef62a1
seidaiegoshi <egoshi.seidai@gmail.com>
2022-12-17 20:42:59 +0900

    product_list画面

--- pages/setting/product_list.php
+++ pages/setting/product_list.php
@@ -1,6 +1,54 @@
 <?php
 include("./../function/footer.php");
+include('./../function/db.php');
+
 $footer = footer();
+
+
+// データベースに接続
+$pdo = connect_to_db();
+
+$sql = 'SELECT * FROM products_table WHERE company_id = :company_id';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':company_id', 1, PDO::PARAM_STR);
+
+
+try {
+  $status = $stmt->execute();
+} catch (PDOException $e) {
+  echo json_encode(["sql error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$table_element = "";
+foreach ($result as $key => $record) {
+  $table_element .= "
+    <tr>
+      <td>{$record["product_name"]}</td>
+      <td>{$record["unit"]}</td>
+      <td class='main_cost'>{$record["main_cost"]}<span class='yen'>円</span></td>
+      <td class='sub_cost'>{$record["sub_cost"]}<span class='yen'>円</span></td>
+      <td class='selling_price'>{$record["selling_price"]}<span class='yen'>円</span></td>
+
+      <td class='td_edit hidden'>
+        <form action='./product_edit.php' method='GET'>
+        <input type='hidden' value='{$record["id"]}'>
+        <button class='button-main'>編集</button>
+        </form>
+      </td>
+      <td class='td_delete hidden'>
+        <form action='./product_delete.php' method='GET'>
+        <input type='hidden' value='{$record["id"]}'>
+        <button class='button-delete'>削除</button>
+        </form>
+      </td>
+    </tr>
+  ";
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -48,7 +96,7 @@ $footer = footer();
   <section class="main">
 
     <div class="search">
-      <input type="text" placeholder="商品名検索">
+      <input id="search" type="text" placeholder="商品名検索">
     </div>
 
     <div class="menu">
@@ -59,10 +107,14 @@ $footer = footer();
       </div>
       <div>
         <div>
-          <a href="" class="button-main">編集</a>
+          <button class="button-main" id="edit-button">
+            編集
+          </button>
         </div>
         <div>
-          <a href="" class="button-delete">削除</a>
+          <button class="button-delete" id="delete-button">
+            削除
+          </button>
         </div>
       </div>
     </div>
@@ -78,15 +130,58 @@ $footer = footer();
             <td></td>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          <?= $table_element ?>
+        </tbody>
       </table>
     </div>
-
   </section>
 
 
-
   <?= $footer ?>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script>
+    $("#search").on("keyup", function(e) {
+      // console.log(e.target.value);
+      const searchWord = e.target.value;
+      const url = "./product_list_ajax.php";
+      axios
+        .get(`${url}?search_word=${searchWord}`)
+        .then(function(response) {
+          htmlElement = "";
+          response.data.forEach((record) => {
+            htmlElement += `<tr>
+            <td>${record.product_name}</td>
+            <td>${record.unit}</td>
+            <td class="main_cost">${record.main_cost}<span class='yen'>円</span></td>
+            <td class="sub_cost" >${record.sub_cost}<span class='yen'>円</span></td>
+            <td class="selling_price" >${record.selling_price}<span class='yen'>円</span></td>
+            <tr>`;
+          });
+          if (htmlElement !== "") {
+            $("tbody").html(htmlElement);
+          } else {
+            $("tbody").html(`<?= $table_element ?>`);
+          }
+          // console.log(htmlElement);
+        })
+        .catch(function(error) {})
+        .finally(function() {});
+    });
+  </script>
+  <script>
+    // 編集・削除ボタン表示
+    $("#edit-button").on("click", function() {
+      $(".td_edit").removeClass("hidden");
+      $(".td_delete").addClass("hidden");
+    })
+    $("#delete-button").on("click", function() {
+      $(".td_delete").removeClass("hidden");
+      $(".td_edit").addClass("hidden");
+    })
+  </script>
+
 </body>
 
 </html>
