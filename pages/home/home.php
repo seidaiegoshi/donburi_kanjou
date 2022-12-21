ef614d12d262ddb5e53b67ec0facee5e1799b7d2
seidaiegoshi <egoshi.seidai@gmail.com>
2022-12-21 19:47:34 +0900

    ホーム画面を作成

--- pages/home/home.php
+++ pages/home/home.php
@@ -13,6 +13,7 @@ $footer = footer("home");
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>home</title>
   <link rel="stylesheet" type="text/css" href="./../../css/basic_style.css">
+  <link rel="stylesheet" type="text/css" href="./../../css/home.css">
   <script src="https://kit.fontawesome.com/66d795ff86.js" crossorigin="anonymous"></script>
 </head>
 
@@ -26,6 +27,47 @@ $footer = footer("home");
     <div>
     </div>
   </header>
+
+  <section class="main">
+    <h2>はじめに</h2>
+    <div>
+      <p>ツールは月の利益目標を可視化するツールです。</p>
+    </div>
+    <h2>使い方</h2>
+    <div>
+      <h3>固定費と商品を登録</h3>
+      <div>
+        <p>はじめに、
+          <a href="">SETTINGページ<i class='fa-solid fa-gear'></i></a>
+          で
+          <a href="">固定費</a>
+          と
+          <a href="">販売している商品</a>
+          を登録します。
+        </p>
+      </div>
+    </div>
+    <div>
+      <h3>毎日の販売数を入力</h3>
+      <div>
+        <p>登録した商品がいくつ売れたかを
+          <a href="./../input/sales_performance.php">入力<i class='fa-solid fa-pen'></i></a>
+          します。
+        </p>
+      </div>
+
+    </div>
+    <div>
+      <h3>グラフを確認</h3>
+      <div>
+        <p>登録した情報をもとに、
+          <a href="">グラフを表示<i class='fa-solid fa-chart-line'></i></a>
+          します。
+        </p>
+      </div>
+    </div>
+
+  </section>
 
 
 
